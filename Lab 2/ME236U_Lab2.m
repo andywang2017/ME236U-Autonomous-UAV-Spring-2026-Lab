ccd99f8064c565c53fb8c805800510a46ba58512
andywang2017 <144642540+andywang2017@users.noreply.github.com>
2026-03-12 23:50:36 -0700

    updated code

--- Lab 2/ME236U_Lab2.m
+++ Lab 2/ME236U_Lab2.m
@@ -21,22 +21,32 @@ figure;
 for i = 1:length(cmdSpd)
     data{i} = extractData(logFiles{i});
     subplot(2,2,i)
-    plot(data{i}.Time, data{i}.AccelZ);
+    dataGraphed = plot(data{i}.Time, data{i}.AccelZ, DisplayName = "Data");
     hold on
     grid on
     xlabel("Time (s)")
     ylabel("Accel Z (m/s^2)")
     
-    xline(data{i}.Time(idxStart(i)), 'r--')
-    xline(data{i}.Time(idxEnd(i)), 'r--')
+    steadyStart = xline(data{i}.Time(idxStart(i)), 'r--', ...
+        DisplayName = "Steady State Start");
+    steadyEnd = xline(data{i}.Time(idxEnd(i)), 'r--', ...
+        DisplayName = "Steady State End");
     title("Radians per second: " + cmdSpd(i));
+    
     
     matTemp = data{i}.AccelZ(idxStart(i):idxEnd(i));
     avgAccelZ(i) = sum(matTemp)/length(matTemp);
-    yline(avgAccelZ(i), 'k', 'LineWidth', 1.5)
+    avgAccelVal = yline(avgAccelZ(i), 'k', 'LineWidth', 1.5, ...
+        DisplayName = "Calculated Steady State Value");
     
     sinBeta(i) = avgAccelZ(i)/g;
     Fp(i) = (m_r * g *sinBeta(i) * (l_r/l) + m_b*g*sinBeta(i))/4;
+    title(sprintf( ...
+        ['Commanded: %g [rads/s]\n' ...
+         'Final Steady State Acceleration: %.3g [m/s^2]'], cmdSpd(i), avgAccelZ(i)));
+    if i == 1
+        legend()
+    end
 end
 
 %%% Fit data from cmdSpd and thrust to 2nd order polynomial
@@ -49,13 +59,13 @@ figure;
 hold on
 grid on
 plot(cmdSpd, Fp, 'bo', 'MarkerFaceColor', 'b', 'DisplayName', 'Experimental Data');
-plot(cmdSpd_fit, Fp_fit, 'r-', 'LineWidth', 2, 'DisplayName', sprintf('Linear Fit: y = %.2e x + %.2e', p(1), p(2)));
+plot(cmdSpd_fit, Fp_fit, 'r-', 'LineWidth', 2, 'DisplayName', ...
+    sprintf('2nd Order Fit: y = %.2e x^2 + %.2e x + %.2e', p(1), p(2), p(3)));
 
 xlabel("Motor Cmd (rad/s)")
 ylabel("Thrust per propeller (N)")
 title("Thrust vs Motor Command")
 legend('Location', 'best')
-fprintf('Linear Fit Equation: Thrust = %g * (Motor Cmd) + %g\n', p(1), p(2));
 
 
 function accelData = extractData(filename)
